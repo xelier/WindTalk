6f20c8d39a52e178e696b6727a522996391c3515
xelier <1047094261@qq.com>
2018-09-10 16:02:14 +0800

    打摆子

--- core/sqlhelper.py
+++ core/sqlhelper.py
@@ -10,6 +10,7 @@ import config.config as config
 global pool
 pool = None
 
+
 def connDatabase():
     global pool
     if pool is None:
@@ -28,7 +29,7 @@ def execute(query, ExecuteNoQuery=False, dictCursor=False):
     """
     conn = connDatabase()
     if dictCursor:
-        cur = conn.cursor(cursorclass=pymysql.cursors.DictCursor)
+        cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
     else:
         cur = conn.cursor()
     ret = None
@@ -58,7 +59,7 @@ def insert_many(sql, valueList):
     conn = connDatabase()
     cur = conn.cursor()
     ret = None
-    # batchCount是字段长度
+    # batchCount是字段长度，
     batchCount = 10000
     bugCount = len(valueList)
     times = bugCount / batchCount
@@ -71,11 +72,12 @@ def insert_many(sql, valueList):
         conn.commit()
         ret = count
     except Exception as e:
-        log.info('Error when execute SQL string. Exception: %s' % e.message)
+        log.info('Error when execute SQL string. Exception: %s' % e)
     finally:
         cur.close()
         conn.close()
     return ret
+
 
 def insert_record(table, paramDict):
     """
@@ -94,18 +96,19 @@ def insert_record(table, paramDict):
         return False
     return True
 
-def update_table(table,conditionDict,updateDict):
+
+def update_table(table, conditionDict, updateDict):
     """
     更新table
     @updateDict:更新数据
     @conditionDict:参数字典，唯一确定记录
     """
     ret = True
-    sql = "update %s set" % table
+    sql = "update %s set " % table
     for key, val in updateDict.items():
         if isinstance(val, int):
             sql += "%s = '%d'," % (key, val)
-        elif isinstance(val,float):
+        elif isinstance(val, float):
             sql += "%s = '%f'," % (key, val)
         else:
             sql += "%s = '%s'," % (key, pymysql.escape_string(val))
@@ -117,10 +120,7 @@ def update_table(table,conditionDict,updateDict):
         elif isinstance(val, float):
             sql += "%s = '%f' and " % (key, val)
         elif isinstance(val, str):
-            val = val.replace("'", "\\'").encode("utf-8")
-            sql += "%s='%s' and " % (key, val)
-        else:
-            sql += "%s = '%s' and " % (key, val.replace("'", "\\'").replace("\\", "\\\\"))
+            sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
     sql = sql[:-5]
     result = execute(sql, ExecuteNoQuery=True)
     if result is None:
@@ -128,11 +128,173 @@ def update_table(table,conditionDict,updateDict):
     return ret
 
 
+def get_record_by_param(table, paramDict):
+    """
+    参数查询
+    @paramDict：查询参数
+    """
+    ret = None
+    sql = "select * from %s where " % table
+    for key, val in paramDict.items():
+        sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+    sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    if result is not None and len(result) > 0:
+        ret = result[0]
+    return ret
+
+
+def get_fields_value(table, paramDict, * args):
+    """
+    查询某些字段，通常参数唯一确定某条记录
+    @paramDict：查询参数
+    @args: 要查询的字段，元组
+    返回字典
+    """
+    ret = {}
+    sql = "select * from %s where " % table
+    for key, val in paramDict.items():
+        sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+    sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    if result is None or len(result) == 0:
+        return None
+    for field in args:
+        ret[field] = result[0][field]
+    return ret
+
+
+def get_field_value(table, paramDict, fieldName):
+    """
+    查询某些字段，通常参数唯一确定某条记录
+    @paramDict：查询参数
+    @args: 要查询的字段，元组
+    返回字典
+    """
+    sql = "select * from %s where " % table
+    for key, val in paramDict.items():
+        sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+    sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    if result is None or len(result) == 0:
+        return None
+    return result[0][fieldName]
+
+
+def delete_record_by_param(table, paramDict):
+    """
+    删除记录
+    @paramDict：查询参数
+    """
+    sql = "delete from %s where " % table
+    for key, val in paramDict.items():
+        sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+    sql = sql[:-5]  # 过滤最后两个空格和‘and’
+    result = execute(sql, ExecuteNoQuery=True)
+
+    if result is None:
+        return False
+    return True
+
+
+def get_all_record_list(table, paramDict ):
+    """
+    查询所有数据
+    :param table:
+    :param paramDict:
+    :param pageid:
+    :param pagesize:
+    :return:
+    """
+    sql = "select * from %s " % table
+    if paramDict is not None and len(paramDict) > 0:
+        sql += "where "
+        for key, val in paramDict.items():
+            sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+        sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    return list(result)
+
+
+def get_all_record_num(table, paramDict):
+    """
+    查询所有数据条数
+    :param table:
+    :param paramDict:
+    :param pagesize:
+    :return:
+    """
+    sql = "select count(*) pagenum from %s " % table
+    if paramDict is not None and len(paramDict) > 0:
+        sql += "where "
+        for key, val in paramDict.items():
+            sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+        sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    ret = int(result[0]['pagenum'])
+    return int(ret)
+
+
+def get_page_list(table, paramDict, pageid, pagesize):
+    """
+    分页查询
+    :param table:
+    :param paramDict:
+    :param pageid:
+    :param pagesize:
+    :return:
+    """
+    sql = "select * from %s " % table
+    if paramDict is not None and len(paramDict) > 0:
+        sql += "where "
+        for key, val in paramDict.items():
+            sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+        sql = sql[:-5]
+    sql += " limit %d, %d" % (pageid*pagesize, pagesize)
+    result = execute(sql, dictCursor=True)
+    return list(result)
+
+
+def get_page_num(table, paramDict, pagesize):
+    """
+    查询有多少页
+    :param table:
+    :param paramDict:
+    :param pagesize:
+    :return:
+    """
+    sql = "select count(*) pagenum from %s " % table
+    if paramDict is not None and len(paramDict) > 0:
+        sql += "where "
+        for key, val in paramDict.items():
+            sql += "%s = '%s' and " % (key, pymysql.escape_string(val))
+        sql = sql[:-5]
+    result = execute(sql, dictCursor=True)
+    ret = int(result[0]['pagenum'])
+    if ret % pagesize > 0:
+        ret = ret/pagesize + 1
+    else:
+        ret = ret/pagesize
+    return int(ret)
+
+
+
 
 
 
 if __name__ == "__main__":
-    execute("insert into user values(null ,'bob','bob',1,'bob','xx')")
+    # insert_record('user', {'username': 'bob', 'password': 'bob', 'sex': 0, 'nickname': '中文', 'profile': 'xxx'})
+    # update_table('user', {'username': 'bob'}, {'nickname': '继续'})
+    # s = get_record_by_param('user', {'username': 'bob'})
 
+    # s = get_fields_value('user', {'username': 'bob'}, 'password', 'profile')
+    # s = get_field_value('user', {'username': 'bob'}, 'password')
+    # s = delete_record_by_param('user', {'username': 'bob'})
+    # s = get_page_list('user', {}, 0, 100)
+    # s = get_page_num('user', {'username': 'bob'}, 100)
+    s = get_all_record_num('user', {'username': 'xelier'})
+    # execute("insert into user values(null ,'bob','bob',1,'bob','xx')")
+    # delete_record_by_param("user", {'userName': 'bob'})
+    print(s)
 
 
